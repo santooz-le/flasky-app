b1f7c57fbc3aca782fe06c88aaa8233ef8f48416
Usuario GitHub <usuario@github.com>
2026-08-31 22:00:00 -0300

    feat: semana-07 - flask-sqlalchemy + modelo User + visual identico ao professor

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
-from flask import Flask, render_template, request, session
+import os
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
-# Chave secreta obrigatória para usar sessions
 app.secret_key = 'minha_chave_super_secreta_123'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Salva o nome na sessão do usuário
-        session['name'] = request.form.get('name')
+        name = request.form.get('name', '').strip()
+        if name:
+            session['name'] = name
+            # Salva no banco se ainda não existir
+            if User.query.filter_by(username=name).first() is None:
+                db.session.add(User(username=name))
+                db.session.commit()
+        return redirect(url_for('index'))
 
-    # Pega o nome da sessão (se existir)
     name = session.get('name')
     return render_template('index.html', name=name)
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
